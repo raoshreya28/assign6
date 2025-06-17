00233d8a72442a04bc1c3e8af7530d8f1dd3db96
raoshreya2804 <141177813+raoshreya28@users.noreply.github.com>
2025-06-17 20:33:50 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Armstrong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int original = num;
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum += digit*digit*digit;
+                num /= 10;
+            }
+            if (sum == original)
+            {
+                Console.WriteLine("It is an Armstrong Number ");
+            }
+            else
+            {
+                Console.WriteLine("It is not an Armstrong Number ");
+            }
+        }
+    }
+}
